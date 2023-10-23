88d853c7652e641dab88d63fea4716d77b43a676
Леонид Кокорев <leotechru@ya.ru>
2023-10-23 20:16:28 +0300

    .

--- Solutions/stepik.org/lesson/1067202/step/5.py
+++ Solutions/stepik.org/lesson/1067202/step/5.py
@@ -1,15 +1,18 @@
-a1, a2, a3, a4 = input().split()
-a1, a2, a3, a4 = int(a1), int(a2), int(a3), int(a4)
-l=[]
-if a2<0:
-    a2-=1
-elif a2>0:
-    a2+=1
+def spisok(arg):
+    a1, a2, a3, a4 = arg.split()
+    a1, a2, a3, a4 = int(a1), int(a2), int(a3), int(a4)
+    l = []
+    it = 0
+    if a2 < 0:
+        a2 -= 1
+    elif a2 > 0:
+        a2 += 1
 
-for i in range(a1,a2,a3):
-    l+=[i]
-if a4!=0:
-    for i in l:
-        if (l.index(i)+1)%a4==0:
-            l.remove(i)
-print(l)
+    for i in range(a1, a2, a3):
+        it+=1
+        if a4==0:
+            l+=[i]
+        elif it%a4!=0:
+            l += [i]
+
+    return l
